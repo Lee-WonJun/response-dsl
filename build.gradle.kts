862eb1f2ef744cb3da7b5e2b292084ec45afd939
WonJun Lee <dldnjs1013@nate.com>
2023-03-08 01:36:09 +0900

    Chore: disable bootJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,3 +38,11 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
