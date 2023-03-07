7b899852b52262a0c92a9ffe5f82efc5b8bb896b
WonJun Lee <dldnjs1013@nate.com>
2023-03-08 01:36:47 +0900

    Feat:make dsl builder and test

---
+++ src/main/kotlin/jun/won/responsedsl/servlet/dsl.kt
@@ -0,0 +1,95 @@
+package jun.won.responsedsl.servlet
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.HttpStatus
+import org.springframework.web.servlet.function.ServerResponse
+
+/*
+    * This file contains the DSL for building ServerResponse objects.
+* */
+
+class HeadersBuilder {
+    private var headers: MutableMap<String, String> = mutableMapOf()
+
+    fun header(init: () -> Pair<String, String>) {
+        headers[init().first] = init().second
+    }
+
+    fun build(): Map<String, String> {
+        return headers
+    }
+}
+
+class ResponseBuilder {
+    private var status: HttpStatus = HttpStatus.OK
+    private var contentType: MediaType? = null
+    private var headersMap: MutableMap<String, String> = mutableMapOf()
+    private var body: Any? = null
+
+    fun status(init: () -> HttpStatus) {
+        status = init()
+    }
+
+    fun contentType(init: () -> MediaType) {
+        contentType = init()
+    }
+
+    fun headers(init: HeadersBuilder.() -> Unit) {
+        val builder = HeadersBuilder()
+        builder.init()
+        headersMap += builder.build().toMutableMap()
+    }
+
+    fun header(init: () -> Pair<String, String>) {
+        headersMap[init().first] = init().second
+    }
+
+
+    fun body(init: () -> Any?) {
+        body = init()
+    }
+
+    private fun ServerResponse.BodyBuilder.bodyBuild(body: Any?): ServerResponse {
+        return if (body != null) {
+            this.body(body)
+        } else {
+            this.build()
+        }
+    }
+
+    private fun ServerResponse.BodyBuilder.headers(headersMap: Map<String,String>): ServerResponse.BodyBuilder {
+        headersMap.forEach { (header, value) -> this.header(header, value) }
+        return this
+    }
+
+    private fun ServerResponse.BodyBuilder.contentType(contentType: MediaType?): ServerResponse.BodyBuilder {
+        if (contentType != null) {
+            this.header(HttpHeaders.CONTENT_TYPE, contentType.toString())
+        }
+        return this
+    }
+
+    fun build(): ServerResponse {
+        return ServerResponse.status(status)
+            .contentType(contentType)
+            .headers(headersMap)
+            .bodyBuild(body)
+    }
+}
+
+fun response(init: ResponseBuilder.() -> Unit): ServerResponse {
+    val builder = ResponseBuilder()
+    builder.init()
+    return builder.build()
+}
+
+fun makeDefaultResponse(status: HttpStatus , contentType:MediaType? = null, headersMap: Map<String, String>? = null): (init:ResponseBuilder.()-> Unit) -> ServerResponse {
+    return { init ->
+        response {
+            this.status { status }
+            contentType?.let { this.contentType { it } }
+            headersMap?.let { this.headers { it.forEach { header { it.key to it.value } } } }
+            init()
+        }
+    }
+}
---
+++ src/test/kotlin/jun/won/responsedsl/servlet/ServletDslTest.kt
@@ -0,0 +1,77 @@
+package jun.won.responsedsl.servlet
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.web.servlet.function.EntityResponse
+import org.springframework.web.servlet.function.ServerResponse
+
+data class Dummy(val name: String, val age: Int)
+class ServletDslTest : FunSpec({
+    test("DSL generates the same result as Fluent API.") {
+        val fluentApi = ServerResponse.status(HttpStatus.OK)
+            .contentType(MediaType.TEXT_PLAIN)
+            .header("X-TEST-HEADER", "TEST-VALUE")
+            .body("Hello World")
+
+        val dsl = response {
+            status { HttpStatus.OK }
+            contentType { MediaType.TEXT_PLAIN }
+            header {"X-TEST-HEADER" to "TEST-VALUE" }
+            body { "Hello World" }
+        }
+
+        val responseEntity: String = (fluentApi as EntityResponse<String>).entity()
+        val dslEntity: String = (dsl as EntityResponse<String>).entity()
+
+        dsl.statusCode() shouldBe fluentApi.statusCode()
+        dsl.headers().toMap() shouldBe fluentApi.headers().toMap()
+        dslEntity shouldBe responseEntity
+    }
+
+    test("Header builder works.") {
+        val fluentApi = ServerResponse.status(HttpStatus.OK)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .header("X-TEST-HEADER", "TEST-VALUE")
+            .body(Dummy("Jun", 25))
+
+        val dsl = response {
+            status { HttpStatus.OK }
+            headers {
+                header { HttpHeaders.CONTENT_TYPE to MediaType.APPLICATION_JSON_VALUE }
+                header { "X-TEST-HEADER" to "TEST-VALUE"}
+            }
+            body { Dummy("Jun", 25) }
+        }
+
+        val responseEntity: Dummy = (fluentApi as EntityResponse<Dummy>).entity()
+        val dslEntity: Dummy = (dsl as EntityResponse<Dummy>).entity()
+
+        dsl.statusCode() shouldBe fluentApi.statusCode()
+        dsl.headers().toMap() shouldBe fluentApi.headers().toMap()
+        dslEntity shouldBe responseEntity
+    }
+
+    test("set Default Dsl Function via makeDefaultResponse") {
+        val ok_json = makeDefaultResponse (HttpStatus.OK, MediaType.APPLICATION_JSON, mapOf("X-TEST-HEADER" to "TEST-VALUE"))
+
+        val dsl = ok_json {
+            body { Dummy("Jun", 25) }
+        }
+
+        val fluentApi = ServerResponse.status(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("X-TEST-HEADER", "TEST-VALUE")
+            .body(Dummy("Jun", 25))
+
+
+        val responseEntity: Dummy = (fluentApi as EntityResponse<Dummy>).entity()
+        val dslEntity: Dummy = (dsl as EntityResponse<Dummy>).entity()
+
+        dsl.statusCode() shouldBe fluentApi.statusCode()
+        dsl.headers().toMap() shouldBe fluentApi.headers().toMap()
+        dslEntity shouldBe responseEntity
+    }
+})
